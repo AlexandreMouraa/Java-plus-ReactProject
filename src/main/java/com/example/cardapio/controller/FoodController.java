ba056cc256516e95c52fa69fd5c3a22a4269c67c
AlexandreMouraa <alexandree.moouraa6@gmail.com>
2023-07-09 02:45:04 -0300

    Back-End finalizado com a adição dos 'CrossOrigin' deixando para '*' nesse projeto

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,6 +1,7 @@
 package com.example.cardapio.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ public class FoodController
     @Autowired
     private FoodRepository repository;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
     public void saveFood(@RequestBody FoodRequestDTO data)
     {
@@ -29,6 +31,7 @@ public class FoodController
         return;
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<FoodResponseDTO> getAll()
     {
